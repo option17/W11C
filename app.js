5780aa48009f2ecadd78f2bb27c7fdb6b14e806c
Your Name <you@example.com>
2022-07-25 18:21:09 -0600

    Fixed picture problem

--- app.js
+++ app.js
@@ -52,28 +52,28 @@ function choosePalkia(details) {
 
 let bulbasaur = {
     name: `bulbasaur`,
-    image_url: `<img src="https://c.tenor.com/mCfgYm2yNiIAAAAM/bulbasaur-pokemon.gif"`,
+    image_url: `https://c.tenor.com/mCfgYm2yNiIAAAAM/bulbasaur-pokemon.gif`,
     health: 200,
     attack: `Vine Whip`
 };
 
 let magikarp = {
     name: `magikarp`,
-    image_url: `<img src="https://c.tenor.com/PwnLxYiXYtwAAAAM/pokemon-magikarp.gif"`,
+    image_url: `https://c.tenor.com/PwnLxYiXYtwAAAAM/pokemon-magikarp.gif`,
     health: 100,
     attack: `Splash`
 };
 
 let meowth = {
     name: `meowth`,
-    image_url: `<img src="https://24.media.tumblr.com/4dc1072bdd110a27a9d0922d3a392c8e/tumblr_mrw6pugd3A1rfjowdo1_500.gif"`,
+    image_url: `https://24.media.tumblr.com/4dc1072bdd110a27a9d0922d3a392c8e/tumblr_mrw6pugd3A1rfjowdo1_500.gif`,
     health: 210,
     attack: `Scratch`
 };
 
 let palkia = {
     name: `palkia`,
-    image_url: `<img src="https://i.pinimg.com/originals/53/c6/27/53c627b49a3878bbf3113410cca70737.gif"`,
+    image_url: `https://i.pinimg.com/originals/53/c6/27/53c627b49a3878bbf3113410cca70737.gif`,
     health: 300,
     attack: `Aura Sphere`
 };
